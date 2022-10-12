bdedda21912bdf7ac36fac526f9fcc4e5f2e9b1a
anoop <anoops.nair@gmail.com>
2022-10-12 20:57:36 +1100

    2nd version of quo procedural

--- quotient_procedural.py
+++ quotient_procedural.py
@@ -1,11 +1,12 @@
-#get the first number
-x = 30
+# modified procedural
+x = input("enter first input")
+print("first values is " + x)
+y = input("enter second input")
+print("second values is " + y)
+z = int(x) // int(y)
 
-#get the second number
-y = 5
-
-#add the numbers and store the result
-z = (x//y)
-
-#print the result
-print (z)
+if int(x) < 0 or int(y) < 0:
+    print("please enter positive values")
+else:
+    print('result')
+    print(z)
