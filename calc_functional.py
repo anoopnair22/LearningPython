a325ae7de3e68fed135dacfc88e9fd35cf053911
anoop <anoops.nair@gmail.com>
2022-10-13 21:04:09 +1100

    1st version of calc functional

--- calc_functional.py
+++ calc_functional.py
@@ -21,13 +21,13 @@ p = input("enter first input: ")
 q = input("enter second input: ")
 
 if int(cmd) == 1:
-    print ("first input" + p, "second input" + q, "answer", add(p,q))
+    print ("first input" + p, "second input" + q, "answer", add(p, q))
 elif int(cmd) == 2:
-    print("first input" + p, "second input" + q, sub(p, q))
+    print("first input" + p, "second input" + q, "answer", sub(p, q))
 elif int(cmd) == 3:
-    print("first input" + p, "second input" + q, mul(p, q))
+    print("first input" + p, "second input" + q, "answer", mul(p, q))
 elif int(cmd) == 4:
-    print("first input" + p, "second input" + q, div(p, q))
+    print("first input" + p, "second input" + q, "answer", div(p, q))
 else:
     print("wrong operator selected")
     exit()
