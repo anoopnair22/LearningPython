9c6551c31a4dc04fa8617f71f484218932ee8b8b
anoop <anoops.nair@gmail.com>
2022-10-20 21:52:47 +1100

    Changed name - updated version of procedural

---
+++ leapyear_procedural.py
@@ -0,0 +1,17 @@
+
+print("Leap year check")
+x = input("enter the year: ")
+
+if int(x) % 400 == 0:
+    print("This is a leap year")
+    exit()
+##elif int(x) % 400 != 0 and int(x) % 100 == 0:
+elif int(x) % 100 == 0:
+    print("THis is a not leap year")
+    exit()
+elif int(x) % 4 == 0:
+    print("This is a leap year")
+    exit()
+else:
+    print("This is not a leap year")
+    exit
