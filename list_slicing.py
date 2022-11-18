c132a5473849862b7f2b9694de01cac5eb31049f
anoop <anoops.nair@gmail.com>
2022-11-18 20:20:25 +1100

    New - list slice simple

---
+++ list_slicing.py
@@ -0,0 +1,9 @@
+my_list = ['Red', 'Green', 'Brown', 'yellow', 1, 2, 3, ['a','b','c']]
+print (my_list)
+print("you entered: ", my_list)
+print("Slice of value after removing first and last character:", my_list[1:-1])
+print("Slice of value after removing first 2 characters: ",  my_list[2:])
+print("Slice of value after removing last 2 characters: ", my_list[:-2])
+print("mid value is",my_list[3])
+print("First half slice is: ",my_list[0:4])
+print("Last half slice is: ", my_list[4:] )
