01398227298e19d285eb55b112373ced0951684d
anoop <anoops.nair@gmail.com>
2022-12-01 20:28:57 +1100

    2nd version - improved version

--- dict_average_marks.py
+++ dict_average_marks.py
@@ -1,21 +1,24 @@
 #print avg marks in  Maths:
 scorecard_dict = {}
-name = input("Enter student name: ")
-score = int(input(f" How much did {name} score in maths: "))
-print(f'{name} scored {score} in Maths')
-scorecard_dict[name] = score
-opt = input(f" Do you want to add another student's score? (Yes/No): ")
-total = score
-i = 1
+#name = input("Enter student name: ")
+#score = int(input(f" How much did {name} score in maths: "))
+#print(f'{name} scored {score} in Maths')
+#scorecard_dict[name] = score
+#opt = input(f" Do you want to add another student's score? (Yes/No): ")
+#total = score
+#i = 1
+opt = "Yes"
 while opt == "Yes":
     name = input("Enter student name: ")
     score = int(input(f" How much did {name} score in maths: "))
+    print(f'{name} scored {score} in Maths')
     opt = input(f" Do you want to add another student's score? (Yes/No): ")
     scorecard_dict[name] = score
-    total = score + total
-    i = i + 1
+#    total = score + total
+#    i = i + 1
 
-avg = total / i
+total = sum(scorecard_dict.values())
+avg = total / len(scorecard_dict)
 print(f'Average marks in maths for this class is {avg}')
 
 
