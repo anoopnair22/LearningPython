6e9731a0b54a4b6fb7b24175552633c3fb4a8a10
anoop <anoops.nair@gmail.com>
2022-12-01 22:03:38 +1100

    3rd version - Leo's version

--- fibon_while.py
+++ fibon_while.py
@@ -1,15 +1,62 @@
 ## fibonacci series program (while)
-x = int(input("Enter a number: "))
-print("you entered <", x, ">")
-y = int(input("How many fibonacci numbers you wish to print? "))
-print("Printing", y, "fibonacci numbers after ", x)
-i = 0
-b = x
+start_num = int(input("Enter a number: "))
+print("you entered <", start_num, ">")
+count = int(input("How many fibonacci numbers you wish to print? "))
+print("Printing", count, "fibonacci numbers after ", start_num)
+#i = 0
+#b = x
+#a = 0
+#print (x)
+#while i < y:
+#    print(b)
+#    c = a + b
+#    a = b
+#    b = c
+#    i=i+1
+
 a = 0
-print (x)
-while i < y:
-    print(b)
-    c = a + b
-    a = b
-    b = c
-    i=i+1
+b = 1
+
+if start_num < 0:
+    if count == 1:
+        print ('0')
+    elif count == 2:
+        print ("0", "1")
+    elif count == 3:
+        print ("0", "1", "1")
+    elif count > 3:
+        print("0", "1", "1")
+        count = count - 3
+        a = 1
+        b = 1
+elif start_num == 0:
+    if count == 1:
+        print('1')
+
+    elif count == 2:
+        print("1", "1")
+    elif count > 2:
+        print("1", "1")
+        count = count - 2
+        a = 1
+        b = 1
+elif start_num == 1:
+    if count == 1:
+        print('1')
+
+    elif count > 1:
+        print("1", "1")
+        count = count - 1
+        a = 1
+        b = 1
+elif start_num > 1:
+    print("hi")
+    print (count)
+    if count > 0:
+        c = a + b
+        if c > start_num:
+            print(c)
+            count = count - 1
+            a = b
+            b = c
+
